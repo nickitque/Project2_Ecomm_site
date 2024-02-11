2f2768d96ce3c03d0b135dc56229b78ea7f7dcaf
Nikita <nickitque.39@gmail.com>
2024-02-11 17:41:32 +0100

    Implemented form and models for orders

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products, Order
 
 
 admin.site.register(Products)
+admin.site.register(Order)
--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -12,3 +12,13 @@ class Products(models.Model):
     description = models.TextField()
     image = models.CharField(max_length=300)
 
+
+class Order(models.Model):
+    items = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=1000)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=2000)
+
