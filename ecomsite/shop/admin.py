7b7c042e34c38517ad3b0559302b5bb418bfe57b
Nikita <nickitque.39@gmail.com>
2024-02-11 21:43:38 +0100

    Little bit customized admin panel

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Products, Order
 
 
-admin.site.register(Products)
+admin.site.site_header = "Ecommerce site"
+admin.site.site_title = "Ecommerce site"
+admin.site.index_title = "Manage ABC shopping"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price')
+
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -14,6 +14,9 @@ class Products(models.Model):
 
 
 class Order(models.Model):
+    def __str__(self):
+        return self.name
+
     items = models.CharField(max_length=1000)
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
